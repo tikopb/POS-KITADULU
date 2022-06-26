cbe8503d16e10612e569dc240d4d1cf59c7afd39
tiko <tiko.pb@gmail.com>
2022-06-26 20:31:48 +0700

    start making process for uom convertion

---
+++ backend/migrations/20220626125132-create-uom-convertion.js
@@ -0,0 +1,49 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('UomConvertions', {
+      UomConvertion_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      isactive: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      org_id:{
+        allowNull:false,
+        type:Sequelize.INTEGER
+      },
+      client_id:{
+        allowNull:false,
+        type:Sequelize.INTEGER
+      },
+      Product_id:{
+        allowNull: false,
+        type:Sequelize.INTEGER
+      },
+      uom_id:{
+        allowNull:false,
+        type:Sequelize.INTEGER
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('UomConvertions');
+  }
+};
---
+++ backend/migrations/20220626131845-add-relation-uom-categories.js
@@ -0,0 +1,68 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addConstraint(
+        'UomConvertions',{
+          fields: ['org_id'],
+          type: 'foreign key',
+          name: 'UomCon_org_contraint',
+          references: { //Required field
+            table: 'Orgs',
+            field: 'Org_id'
+          },
+          onDelete: 'cascade',
+          onUpdate: 'cascade'
+        }
+      ),
+      queryInterface.addConstraint(
+        'UomConvertions',{
+          fields: ['client_id'],
+          type: 'foreign key',
+          name: 'UomCon_client_contraint',
+          references: { //Required field
+            table: 'Clients',
+            field: 'Client_id'
+          },
+          onDelete: 'cascade',
+          onUpdate: 'cascade'
+        }
+      ),
+      queryInterface.addConstraint(
+        'UomConvertions',{
+          fields: ['Product_id'],
+          type: 'foreign key',
+          name: 'UomCon_product_contraint',
+          references: { //Required field
+            table: 'Products',
+            field: 'Product_id'
+          },
+          onDelete: 'cascade',
+          onUpdate: 'cascade'
+        }
+      ),
+      queryInterface.addConstraint(
+        'UomConvertions',{
+          fields: ['client_id'],
+          type: 'foreign key',
+          name: 'UomCon_uom_contraint',
+          references: { //Required field
+            table: 'Uoms',
+            field: 'Uom_id'
+          },
+          onDelete: 'cascade',
+          onUpdate: 'cascade'
+        }
+      )
+      
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.removeConstraint('UomConvertions','UomCon_org_contraint')
+    queryInterface.removeConstraint('UomConvertions','UomCon_client_contraint')
+    queryInterface.removeConstraint('UomConvertions','UomCon_product_contraint')
+    queryInterface.removeConstraint('UomConvertions','UomCon_uom_contraint')
+  }
+};
