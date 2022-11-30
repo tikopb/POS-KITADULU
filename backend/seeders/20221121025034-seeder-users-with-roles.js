2f2441260da2f2c468a07c8c33cbed5a875551ed
Muhammad Charis <muhammad.charis.az@gmail.com>
2022-11-30 13:35:24 +0700

    Auto stash before merge of "PW-28-fe-implementasi-halaman-master-product-category" and "origin/main"

--- backend/seeders/20221121025034-seeder-users-with-roles.js
+++ backend/seeders/20221121025034-seeder-users-with-roles.js
@@ -1,16 +1,46 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return await queryInterface.bulkInsert('Users', [
-      {role_id:1, username:"Jack",  password:"$2b$10$zqsQhg4uAHdY2ILWhiUuzO4iJV4dYB6q9go6rl3uBHdpI6ero3gnK",  createdAt: new Date(), updatedAt: new Date() , org_id: 1, client_id: 1 },
-      {role_id:2, username:"Lisa",  password:"$2b$10$xKO96YjypunL72LIA6HMFuHmDaQDe4IbrLYSO8HdS2cYkph6GV.1O",  createdAt: new Date(), updatedAt: new Date() , org_id: 1, client_id: 1 },
-      {role_id:3, username:"Jenie",  password:"$2b$10$cJ59tgXdB6NMsxt3oPFy6eGKwva1sgcyCAkZktdwuCvb5auIn5jo.",  createdAt: new Date(), updatedAt: new Date() , org_id: 1, client_id: 1 }
+  async up(queryInterface, Sequelize) {
+    return await queryInterface.bulkInsert("Users", [
+      {
+        role_id: 1,
+        username: "jack",
+        name: "Jack",
+        password:
+          "$2b$10$zqsQhg4uAHdY2ILWhiUuzO4iJV4dYB6q9go6rl3uBHdpI6ero3gnK",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        org_id: 1,
+        client_id: 1,
+      },
+      {
+        role_id: 2,
+        username: "Lisa",
+        name: "lisa",
+        password:
+          "$2b$10$xKO96YjypunL72LIA6HMFuHmDaQDe4IbrLYSO8HdS2cYkph6GV.1O",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        org_id: 1,
+        client_id: 1,
+      },
+      {
+        role_id: 3,
+        username: "Jenie",
+        name: "jenie",
+        password:
+          "$2b$10$cJ59tgXdB6NMsxt3oPFy6eGKwva1sgcyCAkZktdwuCvb5auIn5jo.",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        org_id: 1,
+        client_id: 1,
+      },
     ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('Users', null, {});
-  }
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Users", null, {});
+  },
 };
