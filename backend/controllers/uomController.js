d815d7a869e9f7f0cfdf360938db2d0858efb1ef
Tiko Putra <tiko.pb@gmail.com>
2022-12-05 15:05:36 +0700

    create delete update on uom controller

--- backend/controllers/uomController.js
+++ backend/controllers/uomController.js
@@ -1,26 +1,44 @@
 let {Uom, Client, Org} = require('../models') 
 
 module.exports = {
+    /**
+     * Getting data uom with ID
+     * @param {Uom_id} req  
+     */
     Get: async(req,res) => {
-        let data = await Uom.findByPk(res.body.Uom_id)
+        let data = await Uom.findByPk(req.body.Uom_id)
         res.status(200).json({
             data
         })
     },
+    /**
+     * Getting all data base on client id is active return data to list of uom
+     * @param {client_id} req 
+     */
     GetAll: async(req,res) => {
         const {client_id} = req.body
-        const clientM = await Client.getClient(client_id)
-        Uom.findAll({
-            where:{
-                client_id: clientM.client_id,
-                isactive: true
-            }
-        }).then(function(data){
-            res.status(200).json({
-                data
+        try {
+            Uom.findAll({
+                where:{
+                    client_id: client_id,
+                    isactive: true
+                }
+            }).then(function(data){
+                res.status(200).json({
+                    data
+                })
             })
-        })
+        } catch (err) {
+            res.status(500).json({
+                err: `${err.toString()}`,
+            })
+        }
     },
+    /**
+     * Creating data of uom
+     * @param {client_id, name, description, org_id, client_id} client_id 
+     * @param {*} res 
+     */
     Create: async(req,res) => {
         const {client_id, name, description, org_id} = req.body
         Uom.create({
@@ -34,26 +52,37 @@ module.exports = {
             msg: 'uom generated'
         })
     },
+    /**
+     * updating data of uom data with uom_id as parameter mandatory
+     * @param {Uom_id, name, description, isactive} req 
+     * @param {msg, data} res 
+     */
     Update: async(req,res) => {
         const {Uom_id, name, description, isactive} = req.body
-        Uom.Update({
+        let data = await Uom.findByPk(Uom_id)
+        console.log('uom' + data)
+        data.set({
             name: name,
             description: description,
-            isactive: true
-        }).then(function(data){
-            try {
-                data.save()
-                res.status(200).json({
-                    data,
-                    msg: 'data updated'
-                })
-            } catch (err) {
-                res.status(401).json({
-                    msg: err.message
-                })
-            }
+            isactive: isactive
         })
+        try {
+            await data.save()
+            res.status(200).json({
+                data,
+                msg: 'data updated'
+            })
+        } catch (err) {
+            res.status(401).json({
+                msg: err.message
+            })
+        }
     },
+    /**
+     * Deleting data base on uom_id
+     * @param {uom_id} req 
+     * @param {*} res 
+     */
     Delete: async(req,res) => {
         const{Uom_id} = req.body
         let data = await Uom.findByPk(Uom_id)
