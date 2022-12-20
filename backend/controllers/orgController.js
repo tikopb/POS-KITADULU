b17c6ba60c7dafcc9865f301ca13cbe6b44906dd
Tiko Putra <tiko.pb@gmail.com>
2022-12-20 12:00:02 +0700

    organization CRUD

--- backend/controllers/orgController.js
+++ backend/controllers/orgController.js
@@ -2,7 +2,36 @@ let { Org } = require('../models');
 const { Op } = require("sequelize");
 
 module.exports = {
-    GenerateOrganization: async(req, res) => {
+    Get: async(req,res) => {
+        let data = await Org.findByPk(req.body.Org_id)
+        res.status(200).json({
+            status: 'succsess',
+            data
+        })
+    },
+    GetAll: async(req,res) => {
+        let UserCrd = req.user
+        console.log('client'+UserCrd.Client_id)
+        try {
+            Org.findAll({
+                where:{
+                    client_id: UserCrd.Client_id,
+                    isactive: true
+                }
+            }).then(function(data){
+                res.status(200).json({
+                    status: 'succsess',
+                    data
+                })
+            })
+        } catch (err) {
+            res.status(500).json({
+                status: 'erorr',
+                err: `${err.toString()}`,
+            })
+        }
+    },
+    Generate: async(req, res) => {
         const {name, description, address} = req.body;
         const UserCrd = req.user
         try {
@@ -27,7 +56,7 @@ module.exports = {
             }
         }
     },
-    UpdateOrganization: async(req,res) => {
+    Update: async(req,res) => {
         const {name, description, address, org_id, isactive} = req.body;
         let orgData = client.findByPk(org_id)
         try {
@@ -49,6 +78,20 @@ module.exports = {
                 res.status(500)
                 res.send({ status: 'error', msg: "Something went wrong"});
             }
+        }
+    },
+    Delete: async(req,res) => {
+        const {Org_id} = req.body
+        let data = await Org.findByPk(Org_id)
+        try {
+            await data.destroy()
+            res.status(200).json({
+                msg:'data deleted'
+            })
+        } catch (err) {
+            res.status(401).json({
+                msg: err.message
+            })
         }
     }
 }
